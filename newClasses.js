2670551d4bf1bbed7cd3cf48c5e3b4e1d299f098
EvaDragonfly <88310290+EvaDragonfly@users.noreply.github.com>
2021-08-06 10:28:58 +0400

    Add files via upload

--- newClasses.js
+++ newClasses.js
@@ -42,9 +42,8 @@ class Person {
 }
 
 class Student extends Person{
-  constructor(name, id, subjects, type) {
+  constructor(name, id, subjects) {
     super(name, id, subjects);
-    this.type = type;
   }
 
 }
@@ -59,14 +58,13 @@ class Teacher extends Person{
 }
 
 class Group {
-  constructor(id, teacherID, subject, room, dateTime, memberIDs, type) {
+  constructor(id, room, dateTime) {
     this.id = id;
-    this.teacherID = teacherID;
-    this.subject = subject;
+    this.teacherID = '';
+    this.subject = '';
     this.room = room;
     this.dateTime = dateTime;
-    this.memberIDs = memberIDs;
-    this.type = type;
+    this.memberIDs = [];
   }
 
   //დარწმუნებული არ ვარ რო ეს მეთოდი კარგი იდეაა, მარა იყოს ასე
@@ -85,76 +83,232 @@ groups = [];
 students = [];
 teachers = [];
 
-let nathaniel = new Student('ნათანიელ სქაი', '1', [], [],'აბიტურიენტი');
-let annie = new Student('ანი ჩანი', '2', [],[], 'საატესტატო');
+let nathaniel = new Student('ნათანიელ სქაი', '1', ['ქართული', 'ინგლისური', 'მათემატიკა']);
+students.push(nathaniel);
+let annie = new Student('ანი ჩანი', '2', ['ქართული', 'ინგლისური']);
+students.push(annie);
+let marMB = new Student('მარეხი ბათერფლაი', '3', ['ქართული', 'მათემატიკა', 'უნარები']);
+students.push(marMB);
+let ninia = new Student('ნინია იოგა', '5', ['ინგლისური', 'უნარები']);
+students.push(ninia);
+let heleni = new Student('ელენი გლინტვეინი', '4', ['ინგლისური', 'ქართული']);
+students.push(heleni);
+let khatia = new Student('ხატია კომპსონი', '5', ['მათემატიკა', 'უნარები', 'ინგლისური']);
+students.push(khatia);
+let sofie = new Student('სოფი სტავროგინი', '6', ['მათემატიკა', 'ქართული']);
+students.push(sofie);
+let berika = new Student('ბერიკა ევოლუცია', '7', ['group0', 'group1', 'group2'], ['ინგლისური', 'უნარები']);
+students.push(berika);
+let tornike = new Student('თორნიკე ჰაიკინგ', '8', ['მათემატიკა', 'უნარები']);
+students.push(tornike);
+let alexander = new Student('სანდრო პინკფლოიდ', '9', ['ინგლისური', 'უნარები', 'ქართული']);
+students.push(alexander);
 
-let group1 = new Group(1, 'მაია', 'ქართული', '203', 'ორშაბათი, 13:00', [], 'საატესტატო');
+
+let teacher1 = new Teacher("როდამი ლელაძე", "1", "group0", ["ქართული"]);
+let teacher2 = new Teacher("ნინო ადამოვი", "2", "group3", ["ფიზიკა"]);
+let teacher3 = new Teacher("მანონი რუტინული", "3", "group4", ["ქიმია"]);
+let teacher4 = new Teacher("ემი რუდროვი", "4", "group1", ["ინგლისური"]);
+
+let group0 = new Group(
+  "group0",
+  "304",
+  "ორშაბათი, ოთხშაბათი, პარასკევი, 13:20"
+);
+group0.subject = 'ქართული';
+group0.memberIDs = ['1', '5'];
+groups.push(group0);
+
+
+
+let group1 = new Group(
+  "group1",
+  "666",
+  "სამშაბათი, შაბათი, 16:00",
+);
+group1.subject = 'მათემატიკა';
+group1.memberIDs = ['1', '2', '8'];
 groups.push(group1);
-let group2 = new Group(2, 'მაგდა', 'მათემატიკა', '204', 'ორშაბათი, 15:00', [], 'საატესტატო');
+
+let group2 = new Group(
+  "group2",
+  "186",
+  "ორშაბათი, ოთხშაბათი, პარასკევი, 19:40",
+);
+group2.subject = 'ინგლისური';
 groups.push(group2);
 
+let group3 = new Group(
+  "group3",
+  "203",
+  "სამშაბათი, ხუთშაბათი, 11:20",
+);
+group3.subject = 'უნარები';
+groups.push(group3);
+//let geo =
 
-function addStudentToGroup(student, group){
-    if(group.memberIDs.includes(student.id)){
-      console.log('მოსწავლე უკვე დამატებულია');
-    } else if(!student.isFree(group.dateTime, groups)){
-        console.log('მოსწავლე დაკავებულია მოცემულ დროს');
-    } else if(group.isFull()){
-      console.log('ჯგუფში ადგილები უკვე შევსებულია')
-    } else {
-     group.memberIDs.push(student.id);
-     student.groupIDs.push(group.id);
+nathaniel.groupIDs.push('group1');
+nathaniel.groupIDs.push('group2');
+ninia.groupIDs.push('group0');
+ninia.groupIDs.push('group1');
+ninia.groupIDs.push('group2');
+berika.groupIDs.push('group1');
+heleni.groupIDs.push('group0');
+annie.groupIDs.push('group1');
+sofie.groupIDs.push('group1');
+khatia.groupIDs.push('group0');
+alexander.groupIDs.push('group1');
+tornike.groupIDs.push('group1');
+
+let saveData = () => {
+      localStorage.setItem("teachers-data",JSON.stringify(teachers));
+      localStorage.setItem("students-data",JSON.stringify(students));
+      localStorage.setItem("groups-data",JSON.stringify(groups));
+  }
+
+  let getData = () => {
+      let teachersData = localStorage.getItem("teachers-data");
+      teachers = JSON.parse(teachersData);
+
+      let studentsData = localStorage.getItem("students-data");
+      students = JSON.parse(studentsData);
+
+      let groupsData = localStorage.getItem("groups-data");
+      groups = JSON.parse(groupsData);
+  }
+
+
+//pop-up-ის შექმნა ჯავასკრიპტში:
+const popWrap = document.createElement('div');
+popWrap.setAttribute('class', 'popup-wrapper');
+const pop = document.createElement('div');
+pop.setAttribute('class', 'popup');
+//close ღილაკისთვის
+const popClose = document.createElement('div');
+popClose.setAttribute('class', 'popup-close');
+popClose.innerText = 'x';
+//კონტენტი
+const cont = document.createElement('div');
+cont.setAttribute('class','popup-content');
+pop.appendChild(popClose);
+pop.appendChild(cont);
+popWrap.appendChild(pop);
+
+const buttons = document.querySelectorAll('.btn-popup');
+
+//button-ებზე poppup-ის დამატება
+buttons.forEach(button => {
+  button.parentElement.appendChild(popWrap);
+})
+
+const popup = document.querySelector('.popup-wrapper');
+
+const close = document.querySelector('.popup-close');
+
+//pop-up-ის გათიშვის ღილაკისთვის
+close.addEventListener('click', ()=>{
+  popup.style.display = 'none';
+  location.reload();
+})
+
+
+buttons.forEach(button => {
+   button.addEventListener('click', () =>{
+     //ეძებს გრუპს, რომელიც ავირჩიეთ
+     let group = groups.find(e => e.id == button.parentElement.parentElement.parentNode.id);
+     popup.style.display = 'block';
+     popup.style.zIndex = '1';
+     //ქმნის მასივს სტუდენტების, ვისაც არჩეული აქვს ჯგუფის საგანი
+     let studSubj = students.filter(e => e.subjects.includes(group.subject));
+     //ქმნის ul-ს ამ სტუდენტების მასივის ეკრანზე გამოსატანად
+     const ul = document.createElement('ul');
+     const content = document.querySelector('.popup-content');
+     content.append(ul);
+     // გამოაქვს პოპაპში სტუდენტები, ვისაც უნდა საგანი და ჯერ არჩეული არ აქვთ
+     for (student of studSubj){
+       if (!group.memberIDs.includes(student.id)){
+         const li = document.createElement('li');
+         //li-ში ვქმნით div-ს რო ჩაამატოს ტექსტი და button-ი
+         const tag = document.createElement("div");
+         const addbtn = document.createElement('button');
+         addbtn.innerText = 'Add';
+  //       addbtn.setAttribute('class', 'success');
+         addbtn.setAttribute('id', student.id);
+         addbtn.setAttribute('class', 'addbtn');
+         tag.innerText = student.name;
+         tag.appendChild(addbtn);
+         li.appendChild(tag);
+         ul.append(li);
+       }
+     }
+     // თითოეული სტუდენტის add-თვის pop-up-ში
+     const addButtons = document.querySelectorAll('.addbtn');
+     addButtons.forEach(addButton =>{
+       addButton.addEventListener('click', () => {
+         let student = students.find(e=>e.id == addButton.id);
+         if(group.memberIDs.includes(student.id)){
+           alert('მოსწავლე უკვე დამატებულია');
+         } else if(!student.isFree(group.dateTime, groups)){
+             alert('მოსწავლე დაკავებულია მოცემულ დროს');
+         } else if(group.isFull()){
+           alert('ჯგუფში ადგილები უკვე შევსებულია');
+         } else {
+          group.memberIDs.push(student.id);
+          student.groupIDs.push(group.id);
+          console.log('მოსწავლე წარმატებით დაემატა');
+          //შლის იმ li-ს სადაც დამატებული სტუდენტის მონაცემებია
+          //თუმცა, თუ ლოკალსტორეჯიდან წამოიღო მონაცემები, შეიძლება აღარც დაჭირდეს, not sure
+          addButton.parentElement.parentElement.remove();
+         }
+         location.reload();
+       })
+     })
+   })
+})
+
+
+
+//dom-ში პოულობს ჯგუფებს
+const domGroups = Array.from(document.querySelectorAll('.table-2-cols')).concat(Array.from(document.querySelectorAll('.table-1-cols')));
+//თითოეული ჯგუფისთვის გამოაქვს მისი წევრების სახელები
+  groups.forEach(group =>{
+    let g = domGroups.find(e=>e.id == group.id);
+    //ჯგუფის სტუდენტებს ფილტრავს
+    let groupStudents = students.filter(e => e.groupIDs.includes(group.id));
+    //ამატებს li-ებს
+    groupStudents.forEach(stud => {
+      const li = document.createElement('li');
+      //li-ში ვქმნით div-ს რო ჩაამატოს ტექსტი და delete button-ი
+      const tag = document.createElement("div");
+      tag.setAttribute('class', 'studList');
+      const delbtn = document.createElement('button');
+      delbtn.innerText = 'DEL';
+      delbtn.setAttribute('name', stud.id);
+      delbtn.setAttribute('class', 'btn btn-dark delbtn');
+      tag.innerText = stud.name;
+      tag.appendChild(delbtn);
+      li.appendChild(tag);
+      g.children[0].insertBefore(li, g.children[0].children[2]);
+      // if(group.isFull){
+      //
+      // }
+    })
+    if(groupStudents.length >= 7){
+      g.children[0].children[9].children[0].remove();
     }
-}
-
-function deleteStudentFromGroup(student, group){
-    let i = group.memberIDs.indexOf(student.id);
-    group.memberIDs.splice(i,1);
-    let j = student.groupIDs.indexOf(group.id);
-    student.groupIDs.splice(j,1);
-}
-
-addStudentToGroup(nathaniel, group1);
-addStudentToGroup(nathaniel, group2);
-addStudentToGroup(annie, group1);
-
-
-
-
-
-deleteStudentFromGroup(annie, group1);
-console.log(annie.groupIDs);
-console.log(nathaniel.getDateTimes(groups));
-console.log(nathaniel.groupIDs);
-
-
-// console.log(nathaniel.getDateTimes(groups));
-// console.log(nathaniel.getChosenSubjects(groups));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- // //მგონია ეს კლასიც გამოგვადგება, თუ არ დაგვჭირდა, წავშლით ^_^
- // class Subject {
- //   constructor(id, name, teachers = [], students = [], dateTimes) {
- //     this.id = id;
- //     this.name = name;
- //     this.teachers = teachers;
- //     this.students = students;
- //     this.dateTimes = dateTimes;
- //   }
- // }
-
- // groupIDs = [1 , 2];
+    //delete button-ების click event-ი
+      const delButtons = document.querySelectorAll('.delbtn');
+      delButtons.forEach(delButton =>{
+        delButton.addEventListener('click', () => {
+          let student = students.find(e=>e.id == delButton.name);
+          let i = group.memberIDs.indexOf(student.id);
+          group.memberIDs.splice(i,1);
+          let j = student.groupIDs.indexOf(group.id);
+          student.groupIDs.splice(j,1);
+          //შლის იმ li-ს სადაც დამატებული სტუდენტის მონაცემებია
+          //თუმცა, თუ ლოკალსტორეჯიდან წამოიღო მონაცემები, შეიძლება აღარც დაჭირდეს, not sure
+          delButton.parentElement.parentElement.remove();
+          location.reload();
+        })
+      })
+    })
