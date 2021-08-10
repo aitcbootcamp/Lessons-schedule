bca0b6365c61e29ed90c34a11958b46398cd2cee
nikanoza <81360200+nikanoza@users.noreply.github.com>
2021-08-10 09:22:53 +0400

    Update teachers.js

--- teachers.js
+++ teachers.js
@@ -37,9 +37,36 @@ let createTeacherList = () => {
         teachersList.appendChild(row);
     }
 }
+teachers[0].groupIDs.push("1","2");
+let moreTeacherInfo = (event) => {
+    let info = document.querySelector(".teacher-info");
+    info.style.display = "block";
 
-let moreTeacherInfo = () => {
-    //ამ ფუნქციის აღწერის გარეშე აგდებს ერორს
+    let id = event.path[0].nextElementSibling.getAttribute("id");
+    let teacher = teachers.find(teacher => teacher.id === id);
+    info.children[1].textContent = "სახელი: " + teacher.name;
+    info.children[2].textContent = "საგნები: " + teacher.subjects.join(", ");
+
+    let list = document.querySelector(".teacher-info");
+    if(list.children.length > 4 ){
+        let ul = document.querySelector(".ganrigi");
+        ul.remove();
+    }
+    let ul = document.createElement("ul");
+    ul.classList.add("ganrigi");
+    info.appendChild(ul);
+
+    for(let id of teacher.groupIDs){
+        let group = groups.find( group => group.id === id);
+        let li = document.createElement("li");
+        li.textContent = group.subject + " - " + group.dateTime + " - ოთახი: " + group.room;
+        ul.appendChild(li);
+    }
+}
+
+let downInfo = element => {
+    let parent = element.parentElement;
+    parent.style.display = "none";
 }
 
 let deleteTeacher = (event) => {
@@ -67,3 +94,48 @@ function removeTeacher(getClickedElementID) {
     teachers.splice(teachers.indexOf(teacher), 1);
   }
 
+let createTeacher = () => {
+    let modal = document.getElementById("myModal");
+    modal.style.display = "block";
+    let input = document.getElementsByClassName("inputs");
+    for (let i = 0; i < input.length; i++) {
+        input[i].setAttribute("required", "");
+    }
+
+    let span = document.getElementsByClassName("close")[1];
+
+    span.onclick = function () {
+        modal.style.display = "none";
+    };
+}  
+
+let addTeacher = () => {
+    let nameInputValue = document.getElementById("name").value;
+    let surnameInputValue = document.getElementById("surname").value;
+    let fullName = nameInputValue + " " + surnameInputValue;
+    let id = Math.round(new Date().getTime() + Math.random() * 100);
+
+    let checkedItemsArray = [];
+    let checkboxes = document.querySelectorAll("input[type=checkbox]:checked");
+  
+    for (let i = 0; i < checkboxes.length; i++) {
+      checkedItemsArray.push(checkboxes[i].value);
+    }
+  
+    let newTeacher = new Teacher(fullName, id, checkedItemsArray);
+    teachers.push(newTeacher);
+    // saveData();
+    createTeacherList();
+    modal.style.display = "none";
+}  
+
+function handleData() {
+    let form_data = new FormData(document.querySelector("form"));
+    if (!form_data.has("subjects[]")) {
+      document.getElementById("chk_option_error").style.visibility = "visible";
+    } else {
+      document.getElementById("chk_option_error").style.visibility = "hidden";
+      addTeacher();
+    }
+    return false;
+}
